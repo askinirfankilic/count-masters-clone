8cbd76e10f031c29f8bc425c61ba1c18879e35a4
Irfan Askin Kilic <kictakimuhabirsincap@gmail.com>
2021-08-25 21:52:59 +0300

    feat: add gate score system

---
+++ Assets/Scripts/Game/GateScore.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum Operation
+{
+    Multiply,
+    Add
+};
+
+public class GateScore : MonoBehaviour
+{
+    #region Private Fields
+
+    [SerializeField] private Operation operation;
+    [SerializeField] private int value;
+
+    #endregion
+
+    private void Start()
+    {
+        throw new NotImplementedException();
+    }
+}
