177304d19701959f24d95cc002052c0a4f633123
Irfan Askin Kilic <kictakimuhabirsincap@gmail.com>
2021-08-26 01:06:16 +0300

    feat: implement CM virtual camera with lock x axis

---
+++ Assets/Scripts/Unity/LockCameraX.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Cinemachine;
+ 
+
+[ExecuteInEditMode] [SaveDuringPlay] [AddComponentMenu("")] // Hide in menu
+public class LockCameraX : CinemachineExtension
+{
+    [Tooltip("Lock the camera's X position to this value")]
+    public float m_XPosition = 10;
+ 
+    protected override void PostPipelineStageCallback(
+        CinemachineVirtualCameraBase vcam,
+        CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
+    {
+        if (stage == CinemachineCore.Stage.Body)
+        {
+            var pos = state.RawPosition;
+            pos.x = m_XPosition;
+            state.RawPosition = pos;
+        }
+    }
+}
