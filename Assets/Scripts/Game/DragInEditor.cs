9faa213559faf11e83f2919bf8da65cfd3a19dc3
DESKTOP-3AMHAQH\kicta <kictakimuhabirsincap@gmail.com>
2021-08-25 04:07:48 +0300

    feat: add draggable point for bot spawns

---
+++ Assets/Scripts/Game/DragInEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragInEditor : MonoBehaviour
+{
+    [DraggablePoint] public Vector3 spawnPosition;
+    [SerializeField] private GameObject automatedCharacterPrefab;
+
+    #region Public Methods
+
+    public void Spawn()
+    {
+        Instantiate(automatedCharacterPrefab, spawnPosition, Quaternion.identity);
+    }
+
+    #endregion
+}
