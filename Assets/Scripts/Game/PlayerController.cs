30465e5f0df4a31ac1e56b051f36ac38e710b04a
Irfan Askin Kilic <kictakimuhabirsincap@gmail.com>
2021-08-25 16:37:55 +0300

    feat: add playercontroller script

---
+++ Assets/Scripts/Game/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    #region Private Fields
+
+    [SerializeField] private float automatedCharacterCount = 1;
+    [SerializeField] private GameObject automatedCharacterPrefab;
+
+    #endregion
+
+    #region Properties
+
+    public float AutomatedCharacterCount
+    {
+        get => automatedCharacterCount;
+        set => automatedCharacterCount = value;
+    }
+
+    public GameObject AutomatedCharacterPrefab => automatedCharacterPrefab;
+
+    #endregion
+
+
+    #region Unity Methods
+
+    private void Start()
+    {
+        if (automatedCharacterCount > 1)
+        {
+            //starting object spawn routine
+        }
+    }
+
+    private void Update()
+    {
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    private void SpawnAutomatedCharacter(int spawnAmount)
+    {
+        
+        //spawn block
+
+        AutomatedCharacterCount += spawnAmount;
+    }
+
+    private void DismissAutomatedCharacter(int dismissAmount)
+    {
+        //dismiss block
+
+        AutomatedCharacterCount -= dismissAmount;
+    }
+
+    #endregion
+}
