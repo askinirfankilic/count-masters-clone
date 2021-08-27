ea3059113779dc53d4a84c9e799d65119777cbb3
Irfan Askin Kilic <kictakimuhabirsincap@gmail.com>
2021-08-28 01:56:25 +0300

    refact: refactor buzzsaw code

--- Assets/Scripts/Game/BuzzSawAnimation.cs
+++ Assets/Scripts/Game/BuzzSawAnimation.cs
@@ -5,11 +5,24 @@ using UnityEngine;
 
 public class BuzzSawAnimation : MonoBehaviour
 {
-    // Start is called before the first frame update
+    #region Unity Methods
+
     void Start()
     {
-        DOTween.Init();
-        transform.DOLocalRotate(new Vector3(90, 0, 360), 0.3f, RotateMode.FastBeyond360).SetLoops(-1, LoopType.Incremental).SetEase(Ease.Linear);
+        RotateBuzzSaw();
     }
-    
+
+    #endregion
+
+    #region Private Methods
+
+    private void RotateBuzzSaw()
+    {
+        DOTween.Init();
+
+        transform.DOLocalRotate(new Vector3(90, 0, 360), 0.3f, RotateMode.FastBeyond360)
+            .SetLoops(-1, LoopType.Incremental).SetEase(Ease.Linear);
+    }
+
+    #endregion
 }
